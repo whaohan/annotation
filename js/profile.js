279f55095518b4be49b07f247b561641ca48c0e6
whaohan <2778351280@qq.com>
2020-09-23 15:14:54 +0800

    detele the unneccary part of profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,4 @@
-var genderSelect       = null;
-var ageSelect          = null;
-var countrySelect      = null;
-var musicianshipSelect = null;
-
+// add the pieces checkbox
 function updatePiecesLabels(piecesStr) {
     var pieces = piecesStr.split("@")
     for (var i = 0; i < pieces.length - 1; i++) {
@@ -26,44 +22,6 @@ function updatePiecesLabels(piecesStr) {
     }
 }
 
-function addCheckEventListenners() {
-    genderSelect.addEventListener("change", function (event) {
-        enableNextButton();
-    });
-
-    ageSelect.addEventListener("change", function (event) {
-        enableNextButton();
-    });
-
-    countrySelect.addEventListener("change", function (event) {
-        enableNextButton();
-    });
-
-    musicianshipSelect.addEventListener("change", function (event) {
-        enableNextButton();
-    });
-}
-
-function enableNextButton() {
-    var submitButton = document.getElementById('submitButton');
-    submitButton.disabled = !validateProfileForm();
-    console.log(validateProfileForm());
-}
-
-function validateProfileForm() {
-    return genderSelect.value != "" && ageSelect.value != "" && countrySelect.value != "" && getMusicianshipValue() != "";
-}
-
-function getMusicianshipValue() {
-    for(var i = 1; i <= 5; i++) {
-        if($("#musicianshipCheck" + i).is(':checked') == true) {
-            return i;
-        }
-    }
-
-    return "";
-}
-
 function updatePiecesCount(data) {
     var piecesData = data.pieces;
     var annotationData = data.annotations;
@@ -73,24 +31,8 @@ function updatePiecesCount(data) {
 
         var annotation = {};
 
-        annotation.answer1_1 = sessionStorage.getItem("answer1_1");
-        annotation.answer1_2 = sessionStorage.getItem("answer1_2");
-        annotation.answer1_3 = sessionStorage.getItem("answer1_3");
-
-        annotation.answer2_1 = sessionStorage.getItem("answer2_1");
-        annotation.answer2_2 = sessionStorage.getItem("answer2_2");
-        annotation.answer2_3 = sessionStorage.getItem("answer2_3");
-
-        annotation.gender = genderSelect.value;
-        annotation.age = ageSelect.value;
-        annotation.country = countrySelect.value;
-        annotation.musicianship = getMusicianshipValue();
-
         var piecesStr = sessionStorage.getItem("pieces");
         var pieces = piecesStr.split("@");
-
-        // var countStr = sessionStorage.getItem("count");
-        // var count = countStr.split("@");
 
         var valenceStr = sessionStorage.getItem("valence");
         var valence = valenceStr.split("@");
@@ -117,15 +59,8 @@ function submitProfileForm() {
 }
 
 function main() {
-    genderSelect       = document.getElementById('genderSelect');
-    ageSelect          = document.getElementById('ageSelect');
-    countrySelect      = document.getElementById('countrySelect');
-    musicianshipSelect = document.getElementById('musicianshipSelect');
-
     var piecesStr = sessionStorage.getItem("names");
     if(piecesStr != null) {
         updatePiecesLabels(piecesStr);
     }
-
-    addCheckEventListenners();
 }
