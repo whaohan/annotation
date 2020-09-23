3f5afdbe0d8e8bede457116e1131b864881ddeb6
whaohan <2778351280@qq.com>
2020-09-23 17:10:58 +0800

    enable the upldate of complete

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// get the stored data
+var account = sessionStorage.getItem("account");
+var password = sessionStorage.getItem("password");
+var complete = sessionStorage.getItem("complete");
+
+let form = new FormData();
+form.set('account', account);
+form.set('password', password);
+// Submit the form
+let xhr = new XMLHttpRequest();
+xhr.open('POST', 'php/validate.php', true);
+
+xhr.onreadystatechange = function() {	
+    var result = xhr.responseText;
+    if(result == -1) {
+        alert("The account is not found! Please check your account and password and try again.");
+        nextPage("index.html");
+    } else {
+        sessionStorage.setItem("complete", result);
+        document.getElementById('account').innerText = account;
+        document.getElementById("complete").innerText= complete;
+    }
+}
+// Send the request
+xhr.send(form);
