22488611248944aa00495cc43111c1bce09593e1
whaohan <2778351280@qq.com>
2020-09-23 21:40:13 +0800

    try to update complete

--- js/main.js
+++ js/main.js
@@ -10,16 +10,18 @@ form.set('password', password);
 let xhr = new XMLHttpRequest();
 xhr.open('POST', 'php/validate.php', true);
 
-xhr.onreadystatechange = function() {	
-    var result = xhr.responseText;
-    if(result == -1) {
-        alert("The account is not found! Please check your account and password and try again.");
-        nextPage("index.html");
-    } else {
-        sessionStorage.setItem("complete", result);
-        document.getElementById('account').innerText = account;
-        document.getElementById("complete").innerText= complete;
-    }
+xhr.onreadystatechange = function() {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+        var result = xhr.responseText;
+        if(result == -1) {
+            alert("The account is not found! Please check your account and password and try again.");
+            nextPage("index.html");
+        } else {
+            sessionStorage.setItem("complete", result);
+            document.getElementById('account').innerText = account;
+            document.getElementById("complete").innerText= complete;
+        }
+    } 
 }
 // Send the request
 xhr.send(form);
