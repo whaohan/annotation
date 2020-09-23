d1991da8e1c2d0f75720588f3b84ccc18b720348
whaohan <2778351280@qq.com>
2020-09-23 20:30:19 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
-// get the stored data
-var account = sessionStorage.getItem("account");
-var password = sessionStorage.getItem("password");
-var complete = sessionStorage.getItem("complete");
+function updateInfo() {
+    // get the stored data
+    var account = sessionStorage.getItem("account");
+    var password = sessionStorage.getItem("password");
+    var complete = sessionStorage.getItem("complete");
 
-let form = new FormData();
-form.set('account', account);
-form.set('password', password);
-// Submit the form
-let xhr = new XMLHttpRequest();
-xhr.open('POST', 'php/validate.php', true);
+    let form = new FormData();
+    form.set('account', account);
+    form.set('password', password);
+    // Submit the form
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'php/validate.php', true);
 
-xhr.onreadystatechange = function() {	
-    var result = xhr.responseText;
-    if(result == -1) {
-        alert("The account is not found! Please check your account and password and try again.");
-        nextPage("index.html");
-    } else {
-        sessionStorage.setItem("complete", result);
-        document.getElementById('account').innerText = account;
-        document.getElementById("complete").innerText= complete;
+    xhr.onreadystatechange = function() {	
+        var result = xhr.responseText;
+        if(result == -1) {
+            alert("The account is not found! Please check your account and password and try again.");
+            nextPage("index.html");
+        } else {
+            sessionStorage.setItem("complete", result);
+            document.getElementById('account').innerText = account;
+            document.getElementById("complete").innerText= complete;
+        }
     }
+    // Send the request
+    xhr.send(form);
 }
-// Send the request
-xhr.send(form);
+
+setTimeout(updateInfo(), 1000);
