12dde966110bab5966fd899876acdd54e1420455
whaohan <2778351280@qq.com>
2020-09-28 22:08:52 +0800

    debug

--- js/profile.js
+++ js/profile.js
@@ -2,10 +2,17 @@
 function updatePiecesLabels(piecesStr) {
     var pieces = piecesStr.split("@")
     for (var i = 0; i < pieces.length - 1; i++) {
+        var checkbox = document.createElement("div");
+        checkbox.classList.add("custom-control");
+        checkbox.classList.add("custom-checkbox");
+
         var label = document.createElement("label");
         label.classList.add("custom-control-label");
         label.innerHTML = "Piece " + (i+1) + ": " + pieces[i];
-        label.setAttribute("for", input.id);
+        label.setAttribute("for", "knownPieceCheck" + i);
+        checkbox.appendChild(label);
+
+        document.getElementById("knownPiecesCheckboxes").appendChild(checkbox);
     }
 }
 // save the pieces we annotate
@@ -25,6 +32,7 @@ function updatePiecesCount(data) {
         var arousal = arousalStr.split("@");
 
         for (var i = 0; i < pieces.length - 1; i++) {
+            annotation.isKnown = $('#knownPieceCheck' + i).is(':checked');
             annotation.valence = JSON.parse("[" + valence[i].substring(0, valence[i].length - 2) + "]");
             annotation.arousal = JSON.parse("[" + arousal[i].substring(0, arousal[i].length - 2) + "]");
 
@@ -40,6 +48,7 @@ function submitProfileForm() {
 }
 
 function main() {
+    // only for adding checkbox
     var piecesStr = sessionStorage.getItem("names");
     if(piecesStr != null) {
         updatePiecesLabels(piecesStr);
